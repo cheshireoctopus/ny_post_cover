0510c9ae9462d08c2256e6ba02380aaed4030b92
cheshireoctopus <chandlermoisen@gmail.com>
2019-01-13 14:43:01 -0500

    Add cron job

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'open-uri'
 require 'nokogiri'
 require 'twilio-ruby'
+require 'rufus-scheduler'
 
 ACCOUNT_SID = ENV['ACCOUNT_SID']
 AUTH_TOKEN = ENV['AUTH_TOKEN']
@@ -10,16 +11,30 @@ TWILIO_NUMBER = ENV['TWILIO_NUMBER']
 RECIPIENT_NUMBER = ENV['RECIPIENT_NUMBER']
 
 get '/' do
-  client = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN)
-  doc = Nokogiri::HTML(open("https://nypost.com/covers/"))
+  @cover_url = scrape_the_post
+  sms_the_cover(@cover_url)
 
-  @cover_url = doc.css('source')[0]['data-srcset'].split().first
+  erb :index
+end
+
+def scrape_the_post
+  doc = Nokogiri::HTML(open("https://nypost.com/covers/"))
+  doc.css('source')[0]['data-srcset'].split().first
+end
+
+def sms_the_cover(cover_url)
+  client = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN)
 
   message = client.messages.create(
     from: TWILIO_NUMBER,
     to: RECIPIENT_NUMBER,
-    media_url: @cover_url
+    media_url: cover_url
   )
+end
 
-  erb :index
+scheduler = Rufus::Scheduler.new
+
+scheduler.cron('0 8 * * *') do
+  @cover_url = scrape_the_post
+  sms_the_cover(@cover_url)
 end
